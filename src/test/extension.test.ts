a5b6b0f1db34fb9dccaa830623a3068fe83ef2e3
lolloz98 <lorenzocarpaneto@yahoo.it>
2026-09-13 11:29:46 +0100

    Update versions and add a test

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,15 +1,34 @@
 import * as assert from 'assert';
-
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
-// import * as myExtension from '../../extension';
 
-suite('Extension Test Suite', () => {
-	vscode.window.showInformationMessage('Start all tests.');
+suite('Checkpoint Paste', () => {
+    test('copies checkpoint block to clipboard', async () => {
+        const document = await vscode.workspace.openTextDocument({
+            language: 'python',
+            content:
+                'scene.play(foo)\n' +
+                '# checkpoint 1\n' +
+                'scene.play(bar)\n' +
+                'scene.wait()\n' +
+                '# checkpoint 2\n' +
+                'scene.play(baz)\n',
+        });
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
-	});
+        const editor = await vscode.window.showTextDocument(document);
+
+        // Put cursor on "# checkpoint 1"
+        const position = new vscode.Position(1, 2);
+        editor.selection = new vscode.Selection(position, position);
+
+        await vscode.commands.executeCommand(
+            'manimvs.onCursorCheckpointPaste'
+        );
+
+        const clipboard = await vscode.env.clipboard.readText();
+
+        assert.strictEqual(
+            clipboard,
+            '# checkpoint 1\nscene.play(bar)\nscene.wait()\n'
+        );
+    });
 });
